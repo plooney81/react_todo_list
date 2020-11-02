ef7651561e1f14fd0a446d8e354682b1f3f3e474
Pete Looney <petelooney81@gmail.com>
2020-11-02 13:41:26 -0600

    first commit

---
+++ src/ToDo.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class ToDo extends Component{
+    constructor() {
+        super();
+        this.state = {
+            todos: [],
+        }
+    }
+
+}
+
+export default ToDo;
